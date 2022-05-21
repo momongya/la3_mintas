5bcd615e40790d5eea0e3170b47e236576c533d3
momongya <mo5e2ka8@gmail.com>
2022-05-21 19:42:32 +0000

    display error message

--- models.rb
+++ models.rb
@@ -3,11 +3,32 @@ Bundler.require
 
 class User < ActiveRecord::Base
     has_secure_password
-    # validates :name,
-    #     presence: true,
-    #     format: {with: /\A\w+\z/ }
-    # validates :password,
-    #     length: { in: 5..10 }
+    validates :name,
+        uniqueness: { message: "を被りのないものに変更してください" }
+    validates :name,
+        length: { 
+            in: 5..15,
+            message: "を5から15文字の長さで入力してください"
+        }
+    validates :name,
+        format: {
+            with: /\A\w+\z/,
+            message: "を英字で入力してください"
+        }
+    validates :password,
+        format: {
+            with: /(?=.*?[a-z])(?=.*?[0-9])/,
+            message: "に半角英字と半角数字をそれぞれ1文字以上入れてください"
+        },
+        length: { 
+            in: 5..15,
+            message: "を5から15文字の長さで入力してください"
+        }
+    validates :email,
+        format: {
+            with: /\A.+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+\z/, 
+            message: "を形式にあうものを入力してください"
+        }
     
     has_many :group_users
     has_many :groups, :through => :group_users
@@ -21,6 +42,9 @@ class GroupUser < ActiveRecord::Base
 end
 
 class Group < ActiveRecord::Base
+    validates :group_name,
+        presence: { message: "を入力してください" },
+        uniqueness: { message: "を被りのないものに変更してください" }
     has_many :group_users
     has_many :users, :through => :group_users, dependent: :destroy
     has_many :tasks
